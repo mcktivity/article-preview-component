45b2047669a3c36113c45a393662fc07fb62f019
mcktivity <mcktivity@gmail.com>
2020-10-09 15:15:03 +0200

    v1.1 adjust margin to match

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -10,7 +10,7 @@ export default () => (
     >
       Frontendmentor
     </a>
-    . Solved by{" "}
+    . Solution by{" "}
     <a
       href="https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT"
       target="_blank"
