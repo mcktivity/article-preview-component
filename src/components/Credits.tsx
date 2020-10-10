5968c11a30d7f8754a3cf332f9551dd9304766f1
mcktivity <mcktivity@gmail.com>
2020-10-10 14:45:05 +0200

    v1.6.1 font, padding & margin to match design

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -2,21 +2,23 @@ import React from "react";
 
 export default () => (
   <div className="credits">
-    Challenge from{" "}
-    <a
-      href="https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Frontendmentor
-    </a>
-    . Solution by{" "}
-    <a
-      href="https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Mcklien
-    </a>
+    <p>
+      Challenge from{" "}
+      <a
+        href="https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Frontendmentor
+      </a>{" "}
+      &#10022; Solution coded by{" "}
+      <a
+        href="https://www.linkedin.com/in/mcklien-ross/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Mcklien
+      </a>
+    </p>
   </div>
 );
